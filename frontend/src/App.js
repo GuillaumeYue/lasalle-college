b823e132bca10bc7c4b832a3b33fc5b822ecc902
Han Yue <alexyuehan@gmail.com>
2024-11-15 00:55:29 -0500

    Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
+import {Container} from 'react-bootstrap'
+import{BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/products/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
