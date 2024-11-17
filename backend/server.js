99bf108c97b4b0d3cd4c17171e46e939fa6a700c
Han Yue <alexyuehan@gmail.com>
2024-11-17 02:03:44 -0500

    reducer

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler} from './middleware/errorMiddleware.js';
+
 
 
 dotenv.config();
@@ -11,10 +13,14 @@ connectDB();
 
 const app = express();
 
+
 app.get('/' , (req, res) => {
     res.send('API is running')
 })
 app.use('/api/products' , productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running on port ${PORT} in mode of ${process.env.NODE_ENV}`.yellow.bold));
---
+++ frontend/src/constants/productConstants.js
@@ -0,0 +1,6 @@
+
+
+//获取所有产品的reducer
+export const PRODUCT_LIST_REQUEST = 'PRODUCT_LIST_REQUEST'
+export const PRODUCT_LIST_SUCCESS = 'PRODUCT_LIST_SUCCESS'
+export const PRODUCT_LIST_FAIL = 'PRODUCT_LIST_FAIL'
