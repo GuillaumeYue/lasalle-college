2c0dad6aaa81743f1745fdf98b4bc4d036ebb307
Han Yue <alexyuehan@gmail.com>
2024-11-21 15:48:42 -0500

    order details & reducer

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL } from '../constants/orderConstants'
+import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL} from '../constants/orderConstants'
+import { ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST } from '../constants/orderConstants'
 //创建订单Action
 export const createOrder = (order) => async (dispatch, getState) => {
     try{
@@ -21,6 +22,34 @@ export const createOrder = (order) => async (dispatch, getState) => {
     }
     catch (error) {
         dispatch({type: ORDER_CREATE_FAIL, 
+            payload: 
+                error.response && error.response.data.message 
+                ? error.response.data.message 
+                : error.message
+         })
+    }
+}
+
+//获取订单Action
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+    try{
+        dispatch({type: ORDER_DETAILS_REQUEST})
+
+        const {userLogin: {userInfo}} = getState()
+
+        //获取登录成功后的用户信息
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        const {data} = await axios.get(`/api/orders/${id}`, config)
+
+        dispatch({type: ORDER_DETAILS_SUCCESS, payload: data})
+
+    }
+    catch (error) {
+        dispatch({type: ORDER_DETAILS_FAIL, 
             payload: 
                 error.response && error.response.data.message 
                 ? error.response.data.message 
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,4 +1,6 @@
 import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL } from "../constants/orderConstants"
+import axios from "axios"
+import { ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS } from "../constants/orderConstants"
 
 //创建订单reducer
 export const orderCreateReducer = (state = {}, action) =>{
@@ -8,6 +10,20 @@ export const orderCreateReducer = (state = {}, action) =>{
         case ORDER_CREATE_SUCCESS:
             return {loading: false, order: action.payload, success: true}
         case ORDER_CREATE_FAIL:
+            return {loading: false, error: action.payload}
+        default:
+            return state
+    }
+}
+
+//获取订单reducer
+export const orderDetailsReducer = (state = {orderItems: [], shippingAddress: {}}, action ) =>{
+    switch(action.type){
+        case ORDER_DETAILS_REQUEST:
+            return {...state,loading: true}
+        case ORDER_DETAILS_SUCCESS:
+            return {loading: false, order: action.payload, success: true}
+        case ORDER_DETAILS_FAIL:
             return {loading: false, error: action.payload}
         default:
             return state
