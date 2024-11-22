335fd0f2c3d8721fd83ec7b7d8926cfce90f4344
Han Yue <alexyuehan@gmail.com>
2024-11-22 13:09:57 -0500

    image uplaod backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
@@ -7,6 +8,7 @@ import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler} from './middleware/errorMiddleware.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 
 
@@ -22,6 +24,11 @@ app.get('/' , (req, res) => {
 app.use('/api/products' , productRoutes)
 app.use('/api/users' , userRoutes)
 app.use('/api/orders' , orderRoutes)
+app.use('/api/upload' , uploadRoutes)
+
+//upload文件夹作为静态文件
+const __dirname = path.resolve();//将多个路径解析成一个绝对路径
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound)
 app.use(errorHandler)
